59a8ee42fb10de8ba8e8e2b4b65c030dc3820245
RIDVAN SIT <2122120019@iesu.edu.tr>
2023-09-11 11:42:51 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import Counter from "./components/Counter"
+import IncreaseCounter from "./components/IncreaseCounter"
+import DecreaseCounter from "./components/DecreaseCounter"
+import IncreaseByTwo from "./components/IncreaseByTwo"
+
+function App() {
+  return (
+    <div>
+      <Counter/>
+      <IncreaseCounter/>
+      <DecreaseCounter/>
+      <IncreaseByTwo/>
+    </div>
+  );
+}
+
+export default App;
